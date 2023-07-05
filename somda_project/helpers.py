f8b5d3d34e5e5fe179764b87ea0971ca897c9af7
Jonas Stettner <mail@jstet.net>
2023-07-05 02:27:57 +0200

    add v1 to_parquet

--- somda_project/helpers.py
+++ somda_project/helpers.py
@@ -3,6 +3,11 @@ from somda_project.console import console
 import os
 import requests
 import time
+import urllib
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+import bz2
 
 
 def gen_urls_old_wiki():
@@ -41,4 +46,80 @@ def download_file(url):
                     progress = min(downloaded / total_length, 1.0) * 100
                     console.log(f"Progress: {progress:.2f}%, Speed: {speed:.2f} B/s")
                     update_time += 10
-    return filepath
+    return filepath, url["id"]
+
+
+def to_parquet(input_filepath, id):
+    # Define chunk size (adjust as needed)
+    chunk_size = 100000
+
+    # Define input and output file paths
+    output_file = f"output_file_{id}.parquet"
+
+    # Define Parquet schema
+    parquet_schema = pa.schema(
+        [
+            ("wiki_code", pa.string()),
+            ("article_title", pa.string()),
+            ("daily_total", pa.int64()),
+            ("hourly_counts", pa.string()),
+        ]
+    )
+
+    print(f"Starting conversion for {id}")
+    start_time = time.time()
+    last_update_time = start_time
+
+    # Open the input file in chunks
+    with bz2.open(input_filepath, "rt") as file:
+        # Create an empty Parquet file writer
+        parquet_writer = None
+        # Read and process the file in chunks
+        while True:
+            # Read a chunk of data
+            chunk = []
+            for _ in range(chunk_size):
+                line = file.readline()
+                if not line:
+                    break
+                parts = line.strip().split(" ")
+                wikicode = urllib.parse.unquote(parts[0])
+                article_title = urllib.parse.unquote(parts[1])
+
+                daily_total = int(parts[-2])
+                hourly_counts = " ".join(parts[-1])
+
+                chunk.append((wikicode, article_title, daily_total, hourly_counts))
+
+            # Break the loop if no more data is available
+            if not chunk:
+                break
+
+            # Convert the chunk to a Pandas DataFrame
+            df = pd.DataFrame(chunk, columns=["wiki_code", "article_title", "daily_total", "hourly_counts"])
+
+            # Convert the DataFrame to a PyArrow table
+            table = pa.Table.from_pandas(df, schema=parquet_schema)
+            print(df.iloc[0])
+
+            # Initialize the Parquet file writer if it hasn't been created
+            if parquet_writer is None:
+                parquet_writer = pq.ParquetWriter(output_file, parquet_schema)
+
+            # Write the chunk to the Parquet file
+            parquet_writer.write_table(table)
+
+            # Print progress every 10 seconds
+            current_time = time.time()
+            elapsed_time = current_time - last_update_time
+            if elapsed_time >= 10:
+                last_update_time = current_time
+                file_position = file.tell()
+                print(f"Current position in uncompressed file: {round(file_position / (1024 ** 3), 2)} GB")
+
+        # Close the Parquet file writer
+        if parquet_writer is not None:
+            parquet_writer.close()
+
+    print(f"File converted to Parquet: {output_file}")
+    print(f"Conversion time: {round(time.time() - start_time, 2)} seconds")
--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,9 +1,10 @@
-from somda_project.helpers import download_file
+from somda_project.helpers import download_file, to_parquet
+import importlib_resources
 import os
 
 
 def test_download_file():
-    filepath = download_file(
+    filepath, _id = download_file(
         {
             "url": "https://dumps.wikimedia.org/other/pageview_complete/2013/2013-01/pageviews-20130101-user.bz2",
             "id": "20130101",
@@ -11,3 +12,10 @@ def test_download_file():
     )
     assert os.path.isfile(filepath)
     os.remove(filepath)
+
+
+def test_to_parquet():
+    resource_path = importlib_resources.files("tests.data.pageviews_raw").joinpath("pageviews.bz2")
+    path = str(resource_path)  # Convert the Path object to a string
+    print(path)
+    to_parquet(path, "20130101")
